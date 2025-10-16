3a6ae8ee836764fda961439a5a6b209c37da78bf
Tigran <129489371+TigranSo@users.noreply.github.com>
2025-10-16 13:42:17 +0500

    Update settings.py

--- store/settings.py
+++ store/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^_(4agvf9f@4dm*56*z502*by1=t098xyn_tyn=mz2nc&avr2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,9 +118,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #отправка писем на почту
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ToledoBilbao96@gmail.com'
-EMAIL_HOST_PASSWORD = 'pczlkeesthorfenh'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = ''
